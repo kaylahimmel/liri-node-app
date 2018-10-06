c2e75db0a2e043f90640afdbdbf1393c426feb52
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-10-06 15:22:21 -0400

    updated the JS

--- Assets/javascript/keys.js
+++ Assets/javascript/keys.js
@@ -2,11 +2,11 @@
 require('dotenv').config()
 
 // SPOTIFY SEARCH FUNCTION (installed in Terminal so doesn't need a require function to make the API call)--------------------------
-search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+search: function({type: 'artist OR album OR track', query: 'My search query', limit: 20}, callback);
 
 // OMDB require function------------------------------------------------------------------------------------------------------------
 var request = require('request');
-var omdbAPI = "http://www.omdbapi.com/?apikey=[yourkey]&"
+var omdbAPI = 'http://www.omdbapi.com/?apikey=[yourkey]&'
 request(omdbAPI, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -15,7 +15,7 @@ request(omdbAPI, function (error, response, body) {
 
 // OMDB require function------------------------------------------------------------------------------------------------------------
 var request = require('request');
-var omdbAPI = "http://www.omdbapi.com/?apikey=[yourkey]&"
+var omdbAPI = 'http://www.omdbapi.com/?apikey=[yourkey]&'
 request(omdbAPI, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
